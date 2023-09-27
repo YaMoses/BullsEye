73a7c9bc4c4524036807c1e682d1893109d63cae
Elyamus <103440142+Elyamus@users.noreply.github.com>
2023-09-27 13:43:00 +0100

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Yamoses on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
